635445aec4c604e3f49ff550efef4ead02bb90ed
IuanPR <103894461+IuanPR@users.noreply.github.com>
2023-09-21 22:28:03 +0300

    Add some new queries

--- code/queries.sql
+++ code/queries.sql
@@ -17,6 +17,7 @@ WHERE city_id = 1 AND coffee_shop_name = 'Urban Grind'
 -- Select all columns where gender = 'M' and coffee_shop_id = 5 and salary < 20000
 SELECT * FROM employees
 WHERE salary < 20000 AND gender = 'M' AND coffee_shop_id = 5
+
 -- ------------------------------------------------------------------------------------------------
 
 -- IN, NOT IN, IS NULL, BETWEEN --
@@ -44,3 +45,71 @@ WHERE email IS NOT NULL
 -- Select all columns where gender = 'F' and coffee_shop_id = 1 and salary between 10000 and 15000
 SELECT * FROM employees
 WHERE (gender = 'F') AND (coffee_shop_id = 1) AND (salary BETWEEN 10000 AND 15000)
+
+-- ------------------------------------------------------------------------------------------------
+
+-- ORDER BY, LIMIT, DISTINCT, Renaming columns
+
+-- Order by salary ascending 
+SELECT employee_id, first_name, last_name, salary FROM employees
+ORDER BY salary
+
+-- Order by salary descending 
+SELECT employee_id, first_name, last_name, salary FROM employees
+ORDER BY salary DESC 
+
+-- Order by salary ascending with limit
+SELECT employee_id, first_name, last_name, salary FROM employees
+ORDER BY salary
+LIMIT 15
+
+-- select all unique coffe_shop_id 
+SELECT DISTINCT coffee_shop_id FROM employees
+
+-- select coffe_shop_id as CSI
+SELECT DISTINCT coffee_shop_id AS CSI FROM employees
+
+-- ------------------------------------------------------------------------------------------------
+
+-- EXTRACT
+
+-- select individual parts of hire_date
+SELECT
+	hire_date as date,
+	EXTRACT(YEAR FROM hire_date) AS year,
+	EXTRACT(MONTH FROM hire_date) AS month,
+	EXTRACT(DAY FROM hire_date) AS day
+FROM employees;
+
+-- ------------------------------------------------------------------------------------------------
+
+-- UPPER, LOWER, LENGTH, TRIM
+-- UPPER, LOWER, LENGTH, TRIM
+
+-- Uppercase first and last names
+SELECT
+	first_name,
+	UPPER(first_name) AS first_name_upper,
+	last_name,
+	UPPER(last_name) AS last_name_upper
+FROM employees;
+
+-- Lowercase first and last names
+SELECT
+	first_name,
+	LOWER(first_name) AS first_name_upper,
+	last_name,
+	LOWER(last_name) AS last_name_upper
+FROM employees;
+
+-- Return the email and the length of emails
+SELECT
+	email,
+	LENGTH(email) AS email_length
+FROM employees;
+
+-- TRIM
+SELECT
+    LENGTH('     HELLO     ') AS hello_with_spaces,
+LENGTH('HELLO') AS hello_no_spaces,
+    LENGTH(TRIM('     HELLO     ')) AS hello_trimmed;
