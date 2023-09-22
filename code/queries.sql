f85234e54a1616a1b138ca06107520e56f6960a4
IuanPR <103894461+IuanPR@users.noreply.github.com>
2023-09-22 18:29:02 +0300

    Some queries

--- code/queries.sql
+++ code/queries.sql
@@ -84,7 +84,6 @@ FROM employees;
 -- ------------------------------------------------------------------------------------------------
 
 -- UPPER, LOWER, LENGTH, TRIM
--- UPPER, LOWER, LENGTH, TRIM
 
 -- Uppercase first and last names
 SELECT
@@ -113,3 +112,98 @@ SELECT
     LENGTH('     HELLO     ') AS hello_with_spaces,
 LENGTH('HELLO') AS hello_no_spaces,
     LENGTH(TRIM('     HELLO     ')) AS hello_trimmed;
+
+-- ------------------------------------------------------------------------------------------------
+
+-- Concatenation, Boolean expressions, wildcards
+
+-- Concatenate first and last names to create full names
+SELECT first_name, last_name, first_name || ' ' || last_name AS full_name FROM employee
+
+-- Concatenate columns to create a sentence
+SELECT first_name || ' ' || last_name || ' makes ' || salary FROM employees;
+
+-- full_name and salary less than 50000
+SELECT first_name || ' ' || last_name AS full_name, 
+(salary < 50000) AS less_than_50k  -- True or false
+FROM employees;
+
+-- return true or false if the email like .com
+SELECT email, email like '%.com%' FROM employees
+
+-- Select email like .com
+SELECT first_name || ' ' || last_name AS full_name, email FROM employees
+WHERE email like '%.com%'
+
+-- ------------------------------------------------------------------------------------------------
+
+-- SUBSTRING, POSITION, COALESCE
+
+-- Delete the first 4 symbols
+SELECT email, SUBSTRING(email FROM 5) FROM employees
+
+-- Find the position of '@' in the email
+SELECT 
+	email,
+	POSITION('@' IN email)
+FROM employees;
+
+-- SUBSTRING & POSITION to find the email client
+SELECT 
+	email,
+	SUBSTRING(email FROM POSITION('@' IN email))
+FROM employees;
+
+-- COALESCE to fill missing emails with custom value
+SELECT 
+	email,
+	COALESCE(email, 'NO EMAIL PROVIDED')
+FROM employees;
+
+-- ------------------------------------------------------------------------------------------------
+
+-- MIN, MAX, AVG, SUM, COUNT
+
+-- Select the minimum salary
+SELECT MIN(salary) as min_sal
+FROM employees;
+
+-- Select the maximum salary
+SELECT MAX(salary) as max_sal
+FROM employees;
+
+-- Select difference between maximum and minimum salary
+SELECT MAX(salary) - MIN(salary)
+FROM employees;
+
+-- Select the average salary
+SELECT AVG(salary)
+FROM employees;
+
+-- Round average salary to nearest integer
+SELECT ROUND(AVG(salary),0)
+FROM employees;
+
+-- Sum up the salaries
+SELECT SUM(salary)
+FROM employees;
+
+-- Count the number of entries
+SELECT COUNT(*)
+FROM employees;
+
+SELECT COUNT(salary)
+FROM employees;
+
+SELECT COUNT(email)
+FROM employees;
+
+-- summary
+SELECT
+  MIN(salary) as min_sal,
+  MAX(salary) as max_sal,
+  MAX(salary) - MIN(salary) as diff_sal,
+  round(avg(salary), 0) as average_sal,
+  sum(salary) as total_sal,
+  count(*) as num_of_emp
+FROM employees;
